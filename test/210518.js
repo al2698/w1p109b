0648909884e2794952588fe8271ce2b5eeed521b
al2698 <s110910531@student.nqu.edu.tw>
2021-05-18 23:47:55 +0800

    1147

---
+++ test/210518.js
@@ -0,0 +1,10 @@
+let i;
+while (true) {
+    i++;
+    let num = Math.floor(Math.random() * 10);
+    console.log(num);
+    if (num = 5) {
+        console.log("i hit 5... QAQ\n", i);
+        break;
+    }
+}
